d2aaa259b63d5448db8e5c0465d985aa4aa5598c
Ivan Schneider <ivan.schneider@doctolib.com>
2025-07-16 00:31:53 +0200

    No bluetooth modal

--- test/workout_test.rb
+++ test/workout_test.rb
@@ -33,7 +33,7 @@ class WorkoutTest < Minitest::Test
   def test_workout_full_flow
     find_field('Vélo').click
     find_field('Cardio').click
-    attach_file('Workout', File.expand_path('The_Famous_40_20_s.zwo', __dir__))
+    attach_file('Entraînement', File.expand_path('The_Famous_40_20_s.zwo', __dir__))
     click_on 'Démarrer'
     assert_selector '[x-ref="workoutSvg"]', visible: true
   end
@@ -43,7 +43,7 @@ class WorkoutTest < Minitest::Test
     find_field('Cardio').click
     fill_in 'FTP (watts)', with: '200'
     fill_in 'Poids (kg)', with: '75'
-    attach_file('Workout', File.expand_path('The_Famous_40_20_s.zwo', __dir__))
+    attach_file('Entraînement', File.expand_path('The_Famous_40_20_s.zwo', __dir__))
     click_on 'Démarrer'
     visit '/'
     assert_field 'FTP (watts)', with: '200'
