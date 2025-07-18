b8b8e09bc06744cb8dfbe937b269f801946279d7
Ivan Schneider <ivan.schneider@doctolib.com>
2025-07-18 16:23:57 +0200

    rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,17 @@
-require "capybara"
-require "capybara/dsl"
-require "capybara/minitest"
-require "minitest/autorun"
-require "rack"
-require "capybara/cuprite"
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/minitest'
+require 'minitest/autorun'
+require 'rack'
+require 'capybara/cuprite'
 
 Capybara.app =
   Rack::Builder.new do
     use Rack::Static,
-        urls: [""],
-        root: File.expand_path("..", __dir__),
-        index: "index.html"
-    run ->(_env) { [404, { "Content-Type" => "text/plain" }, ["Not Found"]] }
+        urls: [''],
+        root: File.expand_path('..', __dir__),
+        index: 'index.html'
+    run ->(_env) { [404, { 'Content-Type' => 'text/plain' }, ['Not Found']] }
   end
 
 Capybara.register_driver(:cuprite) do |app|
@@ -25,7 +25,7 @@ class CapybaraTestBase < Minitest::Test
   include Capybara::Minitest::Assertions
 
   def setup
-    page.driver.set_cookie("test-env", "true")
-    visit "/"
+    page.driver.set_cookie('test-env', 'true')
+    visit '/'
   end
 end
--- test/workout_test.rb
+++ test/workout_test.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
-require_relative "test_helper"
+require_relative 'test_helper'
 
 class WorkoutTest < CapybaraTestBase
   def test_workout_full_flow
-    find_field("Vélo").click
-    find_field("Cardio").click
+    find_field('Vélo').click
+    find_field('Cardio').click
     attach_file(
-      "workoutFile",
-      File.expand_path("The_Famous_40_20_s.zwo", __dir__),
+      'workoutFile',
+      File.expand_path('The_Famous_40_20_s.zwo', __dir__),
       visible: false
     )
-    click_on "Démarrer"
+    click_on 'Démarrer'
     assert_selector '[x-ref="workoutSvg"]', visible: true
   end
 
   def test_ftp_and_weight_local_storage
-    find_field("Vélo").click
-    find_field("Cardio").click
-    fill_in "FTP (watts)", with: "200"
-    fill_in "Poids (kg)", with: "75"
+    find_field('Vélo').click
+    find_field('Cardio').click
+    fill_in 'FTP (watts)', with: '200'
+    fill_in 'Poids (kg)', with: '75'
     attach_file(
-      "workoutFile",
-      File.expand_path("The_Famous_40_20_s.zwo", __dir__),
+      'workoutFile',
+      File.expand_path('The_Famous_40_20_s.zwo', __dir__),
       visible: false
     )
-    click_on "Démarrer"
-    visit "/"
-    assert_field "FTP (watts)", with: "200"
-    assert_field "Poids (kg)", with: "75"
+    click_on 'Démarrer'
+    visit '/'
+    assert_field 'FTP (watts)', with: '200'
+    assert_field 'Poids (kg)', with: '75'
   end
 end
